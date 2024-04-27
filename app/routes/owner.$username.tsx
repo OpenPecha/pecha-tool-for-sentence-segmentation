155d17901259d2edf79fc907da0df58e16c8999e
tenkus47 <tenkus47@gmail.com>
2024-04-28 01:37:08 +0530

    update tasksList

--- app/routes/owner.$username.tsx
+++ app/routes/owner.$username.tsx
@@ -59,7 +59,6 @@ export function formatDate(dateString) {
 function EachUser() {
   let { user, textcount, reviewedTextCount, tasks, session, totalWordCount } =
     useLoaderData();
-  console.log(tasks);
   const [searchParams, setSearchParams] = useSearchParams();
   let startDate =
     searchParams.get("startDate") ||
@@ -102,84 +101,82 @@ function EachUser() {
           >
             {(tasks_list) => (
               <>
-                {Object.keys(tasks_list).length === 0 ? (
-                  <div>no task available</div>
-                ) : (
-                  <>
-                    <div className="w-full h-11 p-2.5  justify-center items-center gap-28 inline-flex">
-                      <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
-                        Name: {user.username}
-                      </div>
-                      <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
-                        Role : {user.role}
-                      </div>
-                      <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
-                        Task : {textcount}
-                      </div>
-                      <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
-                        Reviewed task : {reviewedTextCount}
-                      </div>
-                      <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
-                        Total word count : {totalWordCount}
-                      </div>
-                    </div>
-                    <div className=" max-h-[60vh] overflow-x-auto relative shadow-md sm:rounded-lg  ">
-                      <table className=" w-full max-h-full  text-sm text-left text-gray-500 dark:text-gray-400  ">
-                        <thead className="text-xs sticky top-0 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                          <tr>
-                            <th scope="col" className="py-3 px-6">
-                              Task
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                              Date & Time
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                              Word Count
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                              Status
-                            </th>
+                <div className="w-full h-11 p-2.5  justify-center items-center gap-28 inline-flex">
+                  <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
+                    Name: {user.username}
+                  </div>
+                  <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
+                    Role : {user.role}
+                  </div>
+                  <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
+                    Task : {textcount}
+                  </div>
+                  <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
+                    Reviewed task : {reviewedTextCount}
+                  </div>
+                  <div className="text-center text-black text-base font-normal font-['Inter'] leading-normal">
+                    Total word count : {totalWordCount}
+                  </div>
+                </div>
+                <div className=" max-h-[60vh] overflow-x-auto relative shadow-md sm:rounded-lg  ">
+                  <table className=" w-full max-h-full  text-sm text-left text-gray-500 dark:text-gray-400  ">
+                    <thead className="text-xs sticky top-0 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                      <tr>
+                        <th scope="col" className="py-3 px-6">
+                          Task
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                          Date & Time
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                          Word Count
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                          Status
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {Object.keys(tasks_list).length === 0 ? (
+                        <div>no task Available</div>
+                      ) : (
+                        tasks_list?.map((task, index) => (
+                          <tr
+                            key={index + "fe"}
+                            onClick={() =>
+                              goto(
+                                `/owner/${user.username}/${task.id}?session=${session}`
+                              )
+                            }
+                            className="bg-white cursor-pointer border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-300 hover:text-white"
+                          >
+                            <td className="py-4 px-6">
+                              {truncateString(task.original_text, 30)}
+                            </td>
+                            <td className="py-4 px-6">
+                              {formatDate(task.modified_on)}
+                            </td>
+                            <td className="py-4 px-6">{task.word_count}</td>
+                            <td className="py-4 px-6">
+                              <span
+                                className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
+                                  task.reviewed === true
+                                    ? "bg-green-100 text-green-800"
+                                    : task.reviewed === false
+                                    ? "bg-blue-100 text-blue-800"
+                                    : "bg-red-100 text-red-800"
+                                }`}
+                              >
+                                {task.reviewed ? "true" : "false"}
+                              </span>
+                            </td>
                           </tr>
-                        </thead>
-                        <tbody>
-                          {tasks_list?.map((task, index) => (
-                            <tr
-                              key={index + "fe"}
-                              onClick={() =>
-                                goto(
-                                  `/owner/${user.username}/${task.id}?session=${session}`
-                                )
-                              }
-                              className="bg-white cursor-pointer border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-300 hover:text-white"
-                            >
-                              <td className="py-4 px-6">
-                                {truncateString(task.original_text, 30)}
-                              </td>
-                              <td className="py-4 px-6">
-                                {formatDate(task.modified_on)}
-                              </td>
-                              <td className="py-4 px-6">{task.word_count}</td>
-                              <td className="py-4 px-6">
-                                <span
-                                  className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-                                    task.reviewed === true
-                                      ? "bg-green-100 text-green-800"
-                                      : task.reviewed === false
-                                      ? "bg-blue-100 text-blue-800"
-                                      : "bg-red-100 text-red-800"
-                                  }`}
-                                >
-                                  {task.reviewed ? "true" : "false"}
-                                </span>
-                              </td>
-                            </tr>
-                          ))}
-                        </tbody>
-                      </table>
-                    </div>
-                    <Outlet context={{ user: user.username }} />
-                  </>
-                )}
+                        ))
+                      )}
+                    </tbody>
+                  </table>
+                </div>
+                <Outlet context={{ user: user.username }} />
               </>
             )}
           </Await>

34a66e948c8c2c251d221ac4ebb55e0dcd1ad12f
tenkus47 <tenkus47@gmail.com>
2024-04-28 01:42:29 +0530

    update

--- app/routes/owner/Component/Pagination.tsx
+++ app/routes/owner/Component/Pagination.tsx
@@ -6,7 +6,7 @@ import { PER_PAGE_TEXT_COUNT } from "~/routes/owner.$username";
 export function PaginationBar({ total }: { total: number }) {
   const [searchParams] = useSearchParams();
   const $skip = Number(searchParams.get("$skip")) || 0;
-  const $top = Number(searchParams.get("$top")) ?? PER_PAGE_TEXT_COUNT;
+  const $top = Number(searchParams.get("$top")) || PER_PAGE_TEXT_COUNT;
   const totalPages = Math.ceil(total / $top);
   const currentPage = Math.floor($skip / $top) + 1;
   const maxPages = 7;
